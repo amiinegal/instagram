11f32ec2a10e1546a30d8c5d8d7db4a6c459fb36
amiinegal <you@example.com>
2019-05-21 15:47:20 +0300

    create new path for image_details

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,8 +6,14 @@ from django.conf import settings
 urlpatterns=[
     url(r'^$',views.home_images,name='homePage'),
     url(r'^search/', views.search_users, name='search_users'),
-    url(r'^image/(\d+)',views.image,name ='image'),
+    url(r'^images/',views.image,name ='images'),
+    url(r'^users/', views.user_list, name = 'user_list'),
     url(r'^new/image$', views.new_image, name='new_image'),
+    #url(r'^edit/profile$', views.edit_profile, name='edit_profile'),
+    url(r'^profile/(?P<username>[0-9]+)$', views.individual_profile_page, name='individual_profile_page'),
+    # url(r'^comment/(?P<image_id>\d+)', views.add_review, name='add_review'),
+    url(r'^image/(\d+)', views.image_detail, name='image-detail'),
+    url(r'^myprofile/$', views.myprofile, name='myprofile'),
 ]
 
 if settings.DEBUG:
